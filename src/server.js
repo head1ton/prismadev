c06379f555fe0fd73e7d2a4d0775464d1e59eb6f
Daehwan Cho <daehwancho@Daehwanui-MacBookPro.local>
2019-07-12 00:18:59 +0900

    sendgrid mail

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
-require("dotenv").config();
+//require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+//console.log(__dirname);
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { GraphQLServer } from "graphql-yoga";
 // logging
 import logger from "morgan";
 import schema from "./schema";
+import { sendSecretMail } from "./utils";
 
+sendSecretMail("daehwan.cho@doctorkitchen.co.kr", "123");
+
+//console.log(process.env.PORT);
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
@@ -11,5 +20,5 @@ const server = new GraphQLServer({ schema });
 server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
-    console.log(`🚌 Server running on port http://localhost:${PORT}`)
+    console.log(`🚌  Server running on port http://localhost:${PORT}`)
 );
