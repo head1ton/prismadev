015058ab8f5a048c95dec14e643d3943b1bcab03
Daehwan Cho <daehwancho@Daehwanui-MacBookPro.local>
2019-07-23 21:27:46 +0900

    relation

--- src/fragments.js
+++ src/fragments.js
@@ -1,25 +1,34 @@
+export const USER_FRAGMENT = `
+    id
+    username
+`;
+
 export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment {
-        id
-        text
-        user {
-            username
-            id
-        }
+    id
+    text
+    user {
+        ${USER_FRAGMENT}
     }
 `;
 
-// export const USER_FRAGEMENT = `
-//     fragment UserParts on User {
-//         id
-//         username
-//         email
-//         firstName
-//         lastName
-//         bio
-//         posts {
-//             id
-//             caption
-//         }
-//     }
-// `;
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post {
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
+        }
+    }
+`;
