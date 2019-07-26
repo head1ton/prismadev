8dedc4e699add6138e498caf1a3ae08e9c3524af
Daehwan Cho <daehwancho@Daehwanui-MacBookPro.local>
2019-07-26 11:18:38 +0900

    prisma

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -4,11 +4,11 @@ export default {
     Mutation: {
         editUser: async (_, args, { request, isAuthenticated }) => {
             isAuthenticated(request);
-            const { username, email, firstName, lastName, bio } = args;
+            const { username, email, firstName, lastName, bio, avatar } = args;
             const { user } = request;
             return prisma.updateUser({
                 where: { id: user.id },
-                data: { username, email, firstName, lastName, bio }
+                data: { username, email, firstName, lastName, bio, avatar }
             });
             // OR (await 를 안해줘도 되는 건 return이 마지막 statement이기 때문에 :
             // 서버가 자동으로 이 promise가 resolve되서 서버에 결과를 전달하길 기다려주기 때문에)
